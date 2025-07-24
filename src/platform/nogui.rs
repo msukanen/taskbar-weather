41efcab659a2ac21f54ca6a9678b5c31dc3b8354
Markku Sukanen <markku.sukanen@gmail.com>
2025-07-24 13:39:43 +0300

    Some commenting, and direct weather data in nogui also into log::info channel.

--- src/platform/nogui.rs
+++ src/platform/nogui.rs
@@ -1,13 +1,22 @@
 use crate::query::weather;
 
+/// GUIless bridge to fetching weather info.
+/// 
+/// If successful, weather info is tossed into `stdout`.
+/// 
+/// # Arguments
+/// - `city`— your location, probably, maybe.
+/// - `country`— country code, e.g. "FI", "UK" (or "GB"), "US", etc.
 pub async fn get_weather(city: &String, country: &String) {
     match weather::get_weather(city, country).await {
         Ok(weather) => {
-            println!("{:.1}⁰C (feels like {:.1}⁰C) at {}, {}",
+            let info = format!("{:.1}⁰C (feels like {:.1}⁰C) at {}, {}",
                 weather["main"]["temp"],
                 weather["main"]["feels_like"],
                 city, country
             );
+            println!("{}", info);
+            log::info!("Weather data: {}", info);
         },
         Err(e) => log::error!("Could not fetch weather data! {:?}", e)
     };
