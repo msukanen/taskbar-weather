4f840f6030fb8137fb1d1ae09935e57da3704cb8
Markku Sukanen <markku.sukanen@gmail.com>
2025-07-24 12:43:19 +0300

    Some added commentary.

--- src/platform/mod.rs
+++ src/platform/mod.rs
@@ -1,6 +1,9 @@
 #[cfg(target_os = "windows")]
 pub mod win;
 
+/// A trait for UI's "stealth mode".
 pub trait HideAndSeek {
+    /// Enter stealth mode.  This is generally a one-way trip,
+    /// but may vary by OS and/or implementation.
     fn stealth(&self);
 }
